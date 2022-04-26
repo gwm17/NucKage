574036ee7f52fa4d9c80874636c623d43b6e1cca
Gordon McCann <gmccann@fsu.edu>
2022-04-26 10:05:18 -0400

    Added in thread argument

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,37 @@
 
 int main(int argc, char** argv)
 {
-	if(argc < 2)
-	{
-		return 1;
-	}
-
 	if(false) //turn on/off testing
 	{
 		NucKage::EnergyLossTest();
 		return 0;
 	}
 	
-	NucKage::Simulator* sim = NucKage::CreateSimulator(3);
-	sim->LoadConfig(argv[1]);
+	int nthreads;
+	std::string role;
+	if(argc < 2)
+	{
+		return 1;
+	}
+	else if (argc == 2)
+	{
+		nthreads = 1;
+		role = argv[1];
+	}
+	else if (argc == 3)
+	{
+		nthreads = std::stoi(argv[1]);
+		role = argv[2];
+	}
+	else
+	{
+		return 1;
+	}
+
+	
+	
+	NucKage::Simulator* sim = NucKage::CreateSimulator(nthreads);
+	sim->LoadConfig(role);
 	NucKage::Timer stopwatch("WholeProgram");
 	sim->Run();
 	std::cout<<"Program duration: "<<stopwatch.ElapsedMilliseconds()<<" ms"<<std::endl;
